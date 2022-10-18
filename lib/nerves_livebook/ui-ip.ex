1916461302e2cc00274a1ff5aa7e12732e7dd698
Per Øyvind Kanestrøm <perokane@gmail.com>
2022-10-18 20:49:07 +0200

    Update lib/nerves_livebook/ui-ip.ex

--- lib/nerves_livebook/ui-ip.ex
+++ lib/nerves_livebook/ui-ip.ex
@@ -68,11 +68,7 @@ defmodule NervesLivebook.UIScreen do
   def handle_info(_, state), do: {:noreply, state}
 
   defp led_program(:internet) do
-    write_only("Connected")
-    Process.sleep(500)
     write_only(get_ip())
-    Process.sleep(10000)
-    clear_screen()
   end
   defp led_program(:lan) do
     write_only("lan")
